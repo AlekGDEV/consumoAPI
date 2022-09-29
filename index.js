f8f79b6d92b45934fc81eb6fdfcd6fe7ccec2e6c
AlekGDEV <AlexandreGDEV@outlook.com>
2022-09-29 11:29:00 -0300

    criando o post e melhorando o delete

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function excluir(id){
-    fetch('http://localhost:8000/compras/'+id,{
+function inserir(){
+    event.preventDefault();
+
+    let dados = {
+        item: item.value,
+        quantidade: parseInt(quantidade.value),
+    };
+
+    fetch('http://localhost:8000/compras/', {
+        method:'POST',
+        body: JSON.stringify(dados),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(resposta => resposta.json())
+        .then(resposta => atualizar_lista());
+
+    form_add.reset();
+}
+
+async function excluir(id){
+    await fetch('http://localhost:8000/compras/'+id,{
         method:'DELETE'
     });
-
-    tabela_compras.innerHTML = '';
     atualizar_lista();
 }
 
 function atualizar_lista(){
-
+    tabela_compras.innerHTML = '';
     fetch('http://localhost:8000/compras')
         .then(function(resposta){
             return resposta.json();
@@ -19,17 +38,11 @@ function atualizar_lista(){
                 tabela_compras.innerHTML += 
                 `
                 <tr>
+                    <td>${cada_item.id}</td>
+                    <td>${cada_item.item}</td>
+                    <td>${cada_item.quantidade}</td>
                     <td>
-                    ${cada_item.id}
-                    </td>
-                    <td>
-                    ${cada_item.item}
-                    </td>
-                    <td>
-                    ${cada_item.quantidade}
-                    </td>
-                    <td>
-                        <button class="btn btn-danger" onclick="excluir(${cada_item.id})">Excluir(${cada_item.id})</button>
+                        <button class="btn btn-danger" onclick="excluir(${cada_item.id})">Excluir</button>
                     </td>
                 </tr>
                 `
